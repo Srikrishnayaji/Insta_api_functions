72e8335c3007fc4971f2fa126cba7da64706efa2
Srikrishnayaji <45878094+Srikrishnayaji@users.noreply.github.com>
2019-05-08 13:29:16 +0530

    Update insta_api.py

--- insta_api.py
+++ insta_api.py
@@ -5,6 +5,9 @@ access_token = #Enter your access token
 post_number  = #Enter post number whose details you want to see
 # NOTE: 0 = latest_post ; total_no_of_posts-1 = first post
 
+post_type = #Enter the typre of the post
+#valuess possibe are 1)images 2)videos
+
 url_for_user_details = f'https://api.instagram.com/v1/users/self/?access_token={access_token}'
 url_for_post_details = f'https://api.instagram.com/v1/users/self/media/recent/?access_token={access_token}'
 
@@ -22,11 +25,11 @@ def get_user_details(url_for_user_details):
                  'followers':followers,'following':following}
     return(user_info)
 
-def get_post_details(url_for_post_details, post_number):
+def get_post_details(url_for_post_details, post_number, post_type):
     r = requests.get(url_for_post_details).json()        #r = json data obtained from the api
     no_of_comments = r['data'][post_number]['comments']['count']
     no_of_likes = r['data'][post_number]['likes']['count']
-    post_link = r['data'][post_number]['images']['low_resolution']['url']
+    post_link = r['data'][post_number][post_type]['standard_resolution']['url']
     caption = r['data'][post_number]['caption']['text']
     post_details = {'post_link':post_link, 'comments_count':no_of_comments, 'likes_count':no_of_likes,
                           'caption':caption}
